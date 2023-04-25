4fa04be52ee094664a7c4a0295cd10c3f62e4c0c
KrishSharma <89947579+KrishtopherWishtopher@users.noreply.github.com>
2023-04-24 21:38:54 -0400

    added dynamic header and footer and nav

---
+++ skripts/dynamic.js
@@ -0,0 +1,41 @@
+/*
+$(function(){
+    $("#dynamicFooter").load(".../shared/footer.html");     // Loads The Footer Element.
+    }());
+    
+    $(function(){
+    $("#dynamicHeader").load(".../shared/header.html"); // Loads The Header Element.
+    }());
+    
+    $(function(){
+    $("#nav1").load(".../shared/nav1.html"); // Loads The nav1 Element.
+    }());
+    
+    $(function(){
+    $("#nav2").load(".../shared/nav2.html"); // Loads The nav2 Element.
+    }());
+*/    
+$(document).ready(function()
+{
+    $("#dynamicFooter").load("./shared/footer.html");
+
+
+});
+$(document).ready(function()
+{
+    $("#dynamicHeader").load("./shared/header.html");
+
+});
+$(document).ready(function()
+{
+
+    $("#nav1").load("./shared/nav1.html");
+
+
+});
+$(document).ready(function()
+{
+
+    $("#nav2").load("./shared/nav2.html");
+
+});
